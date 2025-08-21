9d7f220b849e1c9a84188e85e7dd603061c678f5
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-08-21 18:45:48 +0530

    Create sai.groovy

---
+++ sai.groovy
@@ -0,0 +1,33 @@
+pipeline {
+    agent {
+        label 'jenkins-slave1'
+    }
+    tools {
+        jdk 'jdk'
+        maven 'maven'
+    }
+    environment {
+        APPLICATION_NAME = 'eureka'
+        SONAR_URL = 'http://34.122.117.31:9000'
+        SONAR_TOKEN = credentials('sonar_creds')
+    }
+    stages {
+        stage('build') {
+            steps{
+                echo "Building ${APPLICATION_NAME}"
+                sh "mvn clean package -DskipTests=true"
+            }
+        }
+        stage('codequality') {
+            steps {
+                echo "scanning ${APPLICATION_NAME} code"
+                sh """
+                mvn clean verify sonar:sonar \
+                      -Dsonar.projectKey=eureka-ms \
+                      -Dsonar.host.url=${SONAR_URL} \
+                      -Dsonar.login=${SONAR_TOKEN}
+                """
+            }
+        }
+    }
+}
